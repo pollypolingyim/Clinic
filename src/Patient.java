68ca32fe141d2a2a1ab4b51cd71dde4db3c03985
pollypolingyim <>
2018-02-19 10:59:11 -0500

    5th commit

--- src/Patient.java
+++ src/Patient.java
@@ -1,21 +1,21 @@
 import java.util.List;
 
 public class Patient{
-	protected static int patientCounter;
-	protected int patientID;
-	protected Name fullName;
-	protected Address address;
-	protected int streetNum;
-	protected String dob;
-	protected int primaryDoctorID;
-	protected char gender;
-	protected int householderID;/*the householderID of the patient 
-	whom the patient can use insurance from. If the householderID
-	is the same as the patientID, this patient is a householder.
+	private static int patientCounter;
+	private int patientID;//the PK of the patient table
+	private Name fullName;
+	private Address address;
+	private int streetNum;
+	private String dob;
+	private int primaryDoctorID;
+	private char gender;
+	private int householderID;/*the householderID of the patient 
+	whom the patient can use insurance from.
 	If this number is -1, this patient does not have
-	insurance and therefore not belongs to any household*/
-	protected CoverageType coverageType;
-	protected List<String> medicalRecord;
+	insurance and therefore not belongs to any household.
+	The FK of the householder table*/
+	private CoverageType coverageType;
+	private List<String> medicalRecord;
 	
 	public Patient(Name name,Address add, String dob, int doctor, 
 			char gender, int householderID, CoverageType coverageType){
@@ -30,7 +30,8 @@ public class Patient{
 		medicalRecord = null;
 	}
 	
-	public int findPatientID(String fName, String lName, String dob){
+	public int findPatientID(Name name, String dob){
+		//search the patientID by his name and dob. If not found, return -1
 		return -1;
 	}
 	
